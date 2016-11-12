3099da3babcf2833f38c8b229163b6044ac0ea57
Olivier Coanet <olivier.coanet@gmail.com>
2016-11-12 16:26:06 +0100

    Extract MarketDataPublisher

--- DisruptorExperiments/Program.cs
+++ DisruptorExperiments/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using DisruptorExperiments.Engine.X;
 using DisruptorExperiments.MarketData;
-using DisruptorExperiments.MarketData.V1;
+using DisruptorExperiments.MarketData.V2;
 
 namespace DisruptorExperiments
 {
@@ -15,40 +15,54 @@ namespace DisruptorExperiments
             var engine = new XEngine();
             engine.Start();
 
-            PublishMarketDataUpdates(engine);
+            var publisher = new MarketDataPublisher(engine, securityCount: 50);
 
-            Thread.Sleep(1000);
+            var collectionCountBefore = GC.CollectionCount(0);
+
+            publisher.Run(TimeSpan.FromSeconds(10));
+
+            var collectionCount = GC.CollectionCount(0) - collectionCountBefore;
+            Console.WriteLine($"CollectionCount: {collectionCount}");
 
             engine.Stop();
+
+            Console.WriteLine($"Generated UpdateCount: {publisher.UpdateCount}");
 
             Console.WriteLine("...");
             Console.ReadKey();
         }
 
-        private static void PublishMarketDataUpdates(XEngine engine)
+        private class MarketDataPublisher
         {
-            const int securityCount = 50;
+            private readonly Random _random = new Random();
+            private readonly Stopwatch _stopwatch = new Stopwatch();
+            private readonly MarketDataUpdate _marketDataUpdate = new MarketDataUpdate { UpdateCount = 1 };
+            private readonly long[] _prices;
+            private readonly MarketDataConflater[] _conflacters;
 
-            var random = new Random();
-            var prices = Enumerable.Repeat(500L, securityCount).ToArray();
-            var conflacters = Enumerable.Range(0, securityCount).Select(x => new MarketDataConflater(engine, x)).ToArray();
-            var marketDataUpdate = new MarketDataUpdate { UpdateCount = 1 };
-            var updateCount = 0;
-
-            var duration = Stopwatch.StartNew();
-            while (duration.Elapsed < TimeSpan.FromSeconds(10))
+            public MarketDataPublisher(XEngine targetEngine, int securityCount)
             {
-                var securityId = random.Next(prices.Length);
-                prices[securityId] += 5 - random.Next(10);
-
-                marketDataUpdate.Last = prices[securityId];
-                conflacters[securityId].AddOrMerge(marketDataUpdate);
-
-                updateCount++;
-                Thread.SpinWait(1 + random.Next(1 << 5));
+                _prices = Enumerable.Repeat(500L, securityCount).ToArray();
+                _conflacters = Enumerable.Range(0, securityCount).Select(x => new MarketDataConflater(targetEngine, x)).ToArray();
             }
 
-            Console.WriteLine($"Generated UpdateCount: {updateCount}");
+            public int UpdateCount { get; private set; }
+
+            public void Run(TimeSpan duration)
+            {
+                _stopwatch.Restart();
+                while (_stopwatch.Elapsed < duration)
+                {
+                    var securityId = _random.Next(_prices.Length);
+                    _prices[securityId] += 5 - _random.Next(10);
+
+                    _marketDataUpdate.Last = _prices[securityId];
+                    _conflacters[securityId].AddOrMerge(_marketDataUpdate);
+
+                    UpdateCount++;
+                    Thread.SpinWait(1 + _random.Next(1 << 5));
+                }
+            }
         }
     }
 }
